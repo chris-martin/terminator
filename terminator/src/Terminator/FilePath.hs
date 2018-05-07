fac535ed782f90c8364c718bf5680785952a4734
Chris Martin <ch.martin@gmail.com>
2018-05-07 14:54:43 -0400

    add newtypes for path segments

--- terminator/src/Terminator/FilePath.hs
+++ terminator/src/Terminator/FilePath.hs
@@ -3,7 +3,7 @@
 module Terminator.FilePath
   (
   -- The FilePath type
-    FilePath
+    FilePath, DirName, FileName
 
   -- * The terminators
   , Abs, Rel, File, Dir
@@ -31,12 +31,33 @@ import qualified Data.Sequence as Seq
 import Data.Text (Text)
 import qualified Data.Text as Text
 
-type FilePath b t = Ended (Seq Text) b t
+type FilePath b t = Ended (Seq DirName) b t
 
 type Abs  = ClosedL ()
 type Rel  = Open
-type File = ClosedR Text
+type File = ClosedR FileName
 type Dir  = Open
+
+newtype DirName = DirName PathSegment
+
+newtype FileName = FileName PathSegment
+
+newtype PathSegment = PathSegment Text
+
+textPathSegmentMaybe :: Text -> Maybe PathSegment
+textPathSegmentMaybe = \case
+  "." -> Nothing
+  ".." -> Nothing
+  x -> Just (PathSegment x)
+
+pathSegmentText :: PathSegment -> Text
+pathSegmentText (PathSegment x) = x
+
+dirNameText :: DirName -> Text
+dirNameText (DirName (PathSegment x)) = x
+
+fileNameText :: FileName -> Text
+fileNameText (FileName (PathSegment x)) = x
 
 (/) :: FilePath a b -> FilePath b c -> FilePath a c
 (/) = flip (.)
@@ -47,23 +68,28 @@ root = RightOpen Seq.empty ()
 emptyPath :: FilePath Rel Dir
 emptyPath = OpenEnded Seq.empty
 
-dir :: Text -> FilePath Rel Dir
+dir :: DirName -> FilePath Rel Dir
 dir n = OpenEnded (Seq.singleton n)
 
-file :: Text -> FilePath Rel File
+file :: FileName -> FilePath Rel File
 file n = LeftOpen Seq.empty n
 
 absFileText :: FilePath Abs File -> Text
-absFileText (CloseEnded xs () n) = concatAbs (xs |> n)
+absFileText (CloseEnded xs () n) =
+  concatAbs ((dirNameText <$> xs) |> (fileNameText n))
 
 relFileText :: FilePath Rel File -> Text
-relFileText (LeftOpen xs n) = concatRel (xs |> n)
+relFileText (LeftOpen xs n) =
+  concatRel ((dirNameText <$> xs) |> (fileNameText n))
 
 absDirText :: FilePath Abs Dir -> Text
-absDirText (RightOpen xs ()) = concatAbs xs
+absDirText (RightOpen xs ()) =
+  concatAbs (dirNameText <$> xs)
 
 relDirText :: FilePath Rel Dir -> Text
-relDirText = \case { Nil -> "."; OpenEnded xs -> concatRel xs }
+relDirText = \case
+  Nil -> "."
+  OpenEnded xs -> concatRel (dirNameText <$> xs)
 
 concatAbs, concatRel :: Seq Text -> Text
 concatAbs = Text.concat . ((\x -> ["/", x]) =<<) . toList @Seq
